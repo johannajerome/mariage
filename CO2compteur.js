9b907418c93edbde2956170a465920c24926e299
johannajerome <jeromejohanna2026@gmail.com>
2025-08-27 14:39:11 +0200

    Delete CO2compteur.js

--- CO2compteur.js
+++
@@ -1,8 +0,0 @@
-const totalCO2Saved = 1.47;
-
-function updateCO2Counter(co2Value) {
-    const counter = document.getElementById("co2-counter");
-    counter.textContent = co2Value.toFixed(3);
-}
-
-updateCO2Counter(totalCO2Saved);
